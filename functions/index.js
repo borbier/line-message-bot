5864f66c316ef186827a9e70d3a941de5b7802cb
Yuttakhan Baingen <yuttakhanbaingen@gmail.com>
2019-08-11 16:44:53 +0700

    :sparkles: Trying pushing messages

--- functions/index.js
+++ functions/index.js
@@ -9,25 +9,35 @@ const LINE_HEADER = {
 };
 
 exports.LineBot = functions.https.onRequest((req, res) => {
-  if (req.body.events[0].message.type !== 'text') {
-    return;
-  }
-  reply(req.body);
+  return request({
+    method: `GET`,
+    uri: `https://api.openweathermap.org/data/2.5/weather?units=metric&type=accurate&zip=10330,th&appid=yyyyy`,
+    json: true
+  }).then((response) => {
+    const message = `City: ${response.name}\nWeather: ${response.weather[0].description}\nTemperature: ${response.main.temp}`;
+    return push(res, message);
+  }).catch((error) => {
+    return res.status(500).send(error);
+  });
 });
 
-const reply = (bodyResponse) => {
+const push = (res, msg) => {
   return request({
     method: `POST`,
-    uri: `${LINE_MESSAGING_API}/reply`,
+    uri: `${LINE_MESSAGING_API}/push`,
     headers: LINE_HEADER,
     body: JSON.stringify({
-      replyToken: bodyResponse.events[0].replyToken,
+      to: `zzzzz`,
       messages: [
         {
           type: `text`,
-          text: bodyResponse.events[0].message.text
+          text: msg
         }
-	  ]
+      ]
     })
+  }).then(() => {
+    return res.status(200).send(`Done`);
+  }).catch((error) => {
+    return Promise.reject(error);
   });
-};
+}
